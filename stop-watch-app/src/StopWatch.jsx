c3933271bcaf519bb1208900816b4b7a0f48b807
Kevin Andel Josy <kevinandeljosy11@gmail.com>
2024-10-02 21:49:03 +0530

    Completed stopwatch app

--- stop-watch-app/src/StopWatch.jsx
+++ stop-watch-app/src/StopWatch.jsx
@@ -9,25 +9,45 @@ function StopWatch() {
   const startTimeRef = useRef(0);
 
   useEffect(() => {
+    if(isRunning) {
+      intervalIdRef.current = setInterval(() => {
+        setElapsedTime(Date.now() - startTimeRef.current);
+      }, 10);
+    }
+
+    return () => {
+      clearInterval(intervalIdRef.current);
+    }
 
   }, [isRunning]);
 
   function start() {
-
+    setIsRunning(true);
+    startTimeRef.current = Date.now() - elapsedTime;
   }
 
   function stop() {
-
+    setIsRunning(false);
   }
 
   function reset() {
-
+    setElapsedTime(0);
+    setIsRunning(false);
   }
 
   function formatTime() {
 
+    let hours = Math.floor(elapsedTime / (1000 * 60 * 60));
+    let minutes = Math.floor(elapsedTime / (1000 * 60) % 60);
+    let seconds = Math.floor(elapsedTime / (1000) % 60);
+    let milliseconds = Math.floor((elapsedTime % 1000) / 10);
 
-    return `00:00:00`;
+    hours = String(hours).padStart(2, "0");
+    minutes = String(minutes).padStart(2, "0");
+    seconds = String(seconds).padStart(2, "0");
+    milliseconds = String(milliseconds).padStart(2, "0");
+
+    return `${minutes}:${seconds}:${milliseconds}`;
   }
 
   return(<div className="stopwatch">
